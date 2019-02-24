04483eb91e3353147d1bbc76cbf05527e3aabc88
NBarseyni <nicolas.barseyni@protonmail.com>
2019-02-24 11:15:06 +0100

    Ajout toolbar avec back & titre Activité Recherche avancée

--- app/src/main/java/com/bartholome/voicetransport/AdvancedResearchActivity.java
+++ app/src/main/java/com/bartholome/voicetransport/AdvancedResearchActivity.java
@@ -1,7 +1,10 @@
 package com.bartholome.voicetransport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class AdvancedResearchActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class AdvancedResearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advanced_research);
+
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setTitle("Recherche avancée");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
     }
 }
